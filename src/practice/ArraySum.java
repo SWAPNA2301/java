de99916291b5f00e4db12a3437d40c0bb294aed6
SWAPNA2301 <palchowdhuryswapna@gmail.com>
2025-12-13 23:34:13 +0530

    ArraySum.java

---
+++ src/practice/ArraySum.java
@@ -0,0 +1,13 @@
+package practice;
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
